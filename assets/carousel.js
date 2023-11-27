eba505753ea1ac808b9fc5ef2232d263c65efacd
Till Taeubrich <till.taeubrich@gmail.com>
2023-11-27 16:33:53 +0100

    finish carousel images

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,21 +1,31 @@
 window.addEventListener('load', function () {
   let productCardHeight = `${document.querySelector('.custom__product-card .product-card-img-wrapper > img').offsetHeight}px`
-  console.log('🚀 ~ file: carousel.js:3 ~ productCardHeight:', productCardHeight)
-
   const imgWrappers = document.querySelectorAll('.product-card-img-wrapper')
 
   const addHeight = (e) => {
-    e.target.querySelector('.custom__product-card .carousel').style.height = productCardHeight
-    console.log('in')
+    e.target.querySelector('.carousel').style.height = productCardHeight
+    e.target.querySelector('.flickity-viewport').style.height = productCardHeight
+    e.target.querySelectorAll('.carousel .carousel-cell').forEach(cell => {
+      cell.style.height = productCardHeight
+      cell.querySelector('img').style.height = productCardHeight
+    })
   }
 
   const removeHeight = (e) => {
-    e.target.querySelector('.custom__product-card .carousel').style.height = "0"
-    console.log('out')
+    e.target.querySelector('.carousel').style.height = "0"
+    e.target.querySelector('.flickity-viewport').style.height = "0"
+    e.target.querySelectorAll('.carousel .carousel-cell').forEach(cell => {
+      cell.style.height = "0"
+      cell.querySelector('img').style.height = productCardHeight
+    })
   }
 
   imgWrappers.forEach(item => {
     item.addEventListener('mouseenter', (e) => addHeight(e))
     item.addEventListener('mouseleave', (e) => removeHeight(e))
   })
+
+  addEventListener("resize", () => {
+    productCardHeight = `${document.querySelector('.custom__product-card .product-card-img-wrapper > img').offsetHeight}px`
+  });
 }) 
