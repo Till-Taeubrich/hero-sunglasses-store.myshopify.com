a78643f25985226760a5bde82a8a1b32e1041859
Till Taeubrich <till.taeubrich@gmail.com>
2023-11-28 20:12:26 +0100

    setup all form validation

--- assets/registration-form-validation.js
+++ assets/registration-form-validation.js
@@ -1,7 +1,12 @@
 const form = document.querySelector('.account-registration-form')
-const telInput = document.querySelector("#tel-input");
 const checkbox = document.querySelector("#policy-agreement");
 const submitBtn = document.querySelector("#submit-btn");
+const telInput = document.querySelector('#tel-input')
+const phoneErrorMsg = document.querySelector('.phone-error-msg')
+const pwInput = document.querySelector('#password')
+const pwErrorMsg = document.querySelector('.password-error-msg')
+const pwConfirmInput = document.querySelector('#confirm-password')
+const pwConfirmErrorMsg = document.querySelector('.password-confirmation-error-msg')
 
 checkbox.addEventListener('change', function() {
   if (this.checked) {
@@ -11,17 +16,68 @@ checkbox.addEventListener('change', function() {
   }
 });
 
+const showErrorMsg = (errorMsgElement, elementToView) => {
+  errorMsgElement.classList.remove('hidden')
+  elementToView.scrollIntoView();
+
+  setTimeout(() => {
+    errorMsgElement.classList.add('hidden')
+  }, 5000);
+}
+
 const checkTel = () => {
-  const userTel = telInput.value
-  
+  const userTelLength = telInput.value.length
+  const validation =  userTelLength == 10 ? true : false
+
+  if (!validation) {
+    showErrorMsg(phoneErrorMsg, telInput)
+  }
+
+  return validation
+}
+
+const checkPassword = () => {
+  const userPwLength = pwInput.value.length
+  const validation =  userPwLength >= 5 ? true : false
+
+  if (!validation) {
+    showErrorMsg(pwErrorMsg, pwInput)
+  }
+
+  return validation
+}
+
+const checkConfirmPassword = () => {
+  const userPw = pwInput.value
+  const userConfirmPw = pwConfirmInput.value
+  const validation =  userPw === userConfirmPw ? true : false
+
+  if (!validation) {
+    showErrorMsg(pwConfirmErrorMsg, pwConfirmInput)
+  }
+
+  return validation
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  form.reportValidity()
 
-  // checkTel()
-  // checkPassword()
+  const telValidation = checkTel()
+  if (!telValidation) {
+    return
+  }
 
-  form.submit();
+  const passwordValidation = checkPassword()
+  if (!passwordValidation) {
+    return
+  }
+
+  const confirmPasswordValidation = checkConfirmPassword()
+  if (!confirmPasswordValidation) {
+    return
+  }
+
+  if (telValidation && passwordValidation && confirmPasswordValidation) {
+    form.submit();
+  }
 });
